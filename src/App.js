804dcdd54b7969de1ddb26e3afdf2441eebdb533
drood <dbernstein1987@hotmail.com>
2018-12-13 18:53:47 +1100

    state for searchfield

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import CardList from "./CardList";
+import { robots } from "./robots";
+import SearchBox from "./SearchBox";
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchfield: ""
+        };
+    }
+
+    onSearchChange = e => {
+        this.setState({ searchfield: e.target.value });
+    };
+
+    render() {
+        const filterRobots = this.state.robots.filter(robot => {
+            return robot.username
+                .toLowerCase()
+                .includes(this.state.searchfield.toLowerCase());
+        });
+        return (
+            <div className="tc">
+                <h1>Robofriends</h1>
+                <SearchBox onSearchChange={this.onSearchChange} />
+                <CardList robots={filterRobots} />;
+            </div>
+        );
+    }
+}
+
+export default App;
