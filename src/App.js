24d58e83bcf5a708ac1930497104142f827c0f50
drood <dbernstein1987@hotmail.com>
2019-01-15 19:32:07 +1100

    added styles

--- src/App.js
+++ src/App.js
@@ -2,34 +2,35 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import "./App.css";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: robots,
-            searchfield: ""
-        };
-    }
-
-    onSearchChange = e => {
-        this.setState({ searchfield: e.target.value });
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: ""
     };
+  }
 
-    render() {
-        const filterRobots = this.state.robots.filter(robot => {
-            return robot.username
-                .toLowerCase()
-                .includes(this.state.searchfield.toLowerCase());
-        });
-        return (
-            <div className="tc">
-                <h1>Robofriends</h1>
-                <SearchBox onSearchChange={this.onSearchChange} />
-                <CardList robots={filterRobots} />;
-            </div>
-        );
-    }
+  onSearchChange = e => {
+    this.setState({ searchfield: e.target.value });
+  };
+
+  render() {
+    const filterRobots = this.state.robots.filter(robot => {
+      return robot.username
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f1">Robofriends</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList robots={filterRobots} />;
+      </div>
+    );
+  }
 }
 
 export default App;
