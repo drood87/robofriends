2e9ae64717e2c23d918d8fb87f04a20018f7427b
drood <dbernstein1987@hotmail.com>
2019-01-15 19:50:00 +1100

    added dummy json  data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
+
 import SearchBox from "./SearchBox";
 import "./App.css";
 
@@ -8,9 +8,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => {
+        return response.json();
+      })
+      .then(users => {
+        this.setState({ robots: users });
+      });
   }
 
   onSearchChange = e => {
@@ -23,13 +33,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">Robofriends</h1>
-        <SearchBox onSearchChange={this.onSearchChange} />
-        <CardList robots={filterRobots} />;
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">Robofriends</h1>
+          <SearchBox onSearchChange={this.onSearchChange} />
+          <CardList robots={filterRobots} />;
+        </div>
+      );
+    }
   }
 }
 
